71a7d517f21ed2953c458d13f68dac023bf9adfa
Rhavid Gandra <rhavidfonseca@visual.com>
2024-11-24 15:20:42 -0300

    estantes

--- biblioteca/src/App.js
+++ biblioteca/src/App.js
@@ -4,6 +4,9 @@ import service from "./services/index";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [books, setBooks] = useState([{ id: "1", title: "A" }]);
+  const [estante1, setEstante1] = useState([]);
+  const [estante2, setEstante2] = useState([]);
   async function getBooks() {
     try {
       const result = await service.getBooks();
@@ -13,10 +16,33 @@ function App() {
     }
   }
 
+  function adicionarEstante1(book) {
+    setEstante1((prev) => [...prev, book]);
+  }
+
+  function adicionarEstante2(book) {
+    setEstante2((prev) => [...prev, book]);
+  }
+
   useEffect(() => {}, []);
   return (
     <div className="App">
-      <div></div>
+      <div>
+        {books &&
+          books.map((book) => {
+            return (
+              <>
+                <h1>{book.title}</h1>
+                <button type="button" onClick={() => adicionarEstante1(book)}>
+                  Adicionar a estante 1
+                </button>
+                <button type="button" onClick={() => adicionarEstante2(book)}>
+                  Adicionar a estante 2
+                </button>
+              </>
+            );
+          })}
+      </div>
     </div>
   );
 }
